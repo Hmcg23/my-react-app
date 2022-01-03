e1dc6176ca4962b8c9998b34503a8aa214ac068d
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-01-03 10:34:58 -0800

    finish basic counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 0 }
+    this.increaseCounter = this.increaseCounter.bind(this);
+    this.decreaseCounter = this.decreaseCounter.bind(this);
+  }
+
+  increaseCounter() {
+    this.setState({ number: this.state.number + 1})
+  }
+
+  decreaseCounter() {
+    this.setState({ number: this.state.number - 1})
+  }
+
+
+  render() {
+    return (
+    <div className='app'>
+      <h1>Clicker Application</h1>
+      <br/>
+      <h2>{this.state.number}</h2>
+      <br />
+      <div className='button'>
+        <button onClick={this.increaseCounter}>+</button>
+        <button onClick={this.decreaseCounter}>-</button>
+      </div>
     </div>
-  );
+    
+    );
+  }
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
